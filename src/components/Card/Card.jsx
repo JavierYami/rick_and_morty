fbea22d9e479d276fad7dc30c998322c76554656
JavierYami <javieyami1@outlook.com>
2023-01-18 20:37:53 -0600

    estilos basicos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
+import style from '../Card/Card.module.css'
+
 export default function Card(props) {
    return (
-      <div>
+      <div className={style.div}>
          <button onClick={props.onClose}>X</button>
          <h2>{props.name}</h2>
          <h2>{props.species}</h2>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,9 @@
 import SearchBar from '../SearchBar/SearchBar'
+import style from '../Nav/Nav.module.css'
 
 export default function Nav(props){
     return(
-        <nav>
+        <nav className={style.nav}>
             <SearchBar></SearchBar>
         </nav>
     )
